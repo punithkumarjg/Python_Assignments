8094b31c1329106d43975f616da355e2696aa55b
Punith Kumar j g <punithkumarjg@gmail.com>
2023-03-12 12:11:59 +0530

    add the new file

---
+++ Assignments.py
@@ -0,0 +1,34 @@
+#1. Write a command to get the Python version you are using.
+pythoon --version
+#or
+import sys
+print(sys.version)
+#2. Write a python script to print Hello Python on the screen directly in the command line.
+print('Hello Python')
+#3. Write a python script and store it in a file and execute the file from the command line.
+pyhton Assignments.py
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+print("Hello Python")
+#5. Write a python script to print Hello on the first line and Python on the second line
+print('hello','python',sep ='\n')
+#or
+print('Hello')
+print('Python')
+#6. Write a python script to print “MySirG” on the screen
+print('\"MysirG\"')
+#7. Write a python script to print “Teacher’s Day” on the screen
+print("\"Teacher’s Day\"")
+#8. Write a python script to print the value of a variable. Variable contains your name as data.
+name = 'PUNITH'
+print(name)
+#9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name = 'PUNITH'
+age = 23
+qualification = 'xyz'
+years_of_experience = 2
+print(name,age,qualification,years_of_experience,sep ='\n')
+#10. Write a python script to print values of three variables, each in a new line. Variable contains integer values
+n = 1.2
+a = 23
+q = 'xyz'
+print(n,a,q,sep ='\n')
